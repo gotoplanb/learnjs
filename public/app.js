5978b333b2db1a329581f72a546b49d63055530f
Dave Stanton <davestanton.us@gmail.com>
2016-11-16 15:39:58 -0500

    Chapter 2 - Router Function.

---
+++ public/app.js
@@ -0,0 +1,7 @@
+'strict';
+var learnjs = {};
+
+learnjs.showView = function(hash) {
+  var problemView = $('<div class="problem-view">Coming soon!</div>');
+  $('.view-container').empty().append(problemView);
+};
