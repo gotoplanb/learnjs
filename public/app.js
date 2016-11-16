0bb40c965a4263665321743c6bd4b9c1f1fe03ad
Dave Stanton <davestanton.us@gmail.com>
2016-11-16 15:58:14 -0500

    Chapter 2 - Adding Routes.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,21 @@
-'strict';
+'use strict';
+
 var learnjs = {};
 
+learnjs.problemView = function() {
+  return $('<div class="problem-view">').text('Coming soon!');
+}
+
 learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">Coming soon!</div>');
-  $('.view-container').empty().append(problemView);
+
+  var routes = {
+    '#problem-1': learnjs.problemView
+  }
+
+  var viewFn = routes[hash];
+
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
+
 };
