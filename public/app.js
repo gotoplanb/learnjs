841712fdd9894b09f003f27dd7389b832a082a4c
Dave Stanton <davestanton.us@gmail.com>
2016-11-17 13:57:08 -0500

    Fix tests.

--- public/app.js
+++ public/app.js
@@ -9,19 +9,14 @@ learnjs.problemView = function(problemNumber) {
 }
 
 learnjs.showView = function(hash) {
-
   var routes = {
     '#problem': learnjs.problemView
   }
-
   var hashParts = hash.split('-');
-
   var viewFn = routes[hashParts[0]];
-
   if (viewFn) {
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
-
 };
 
 learnjs.appOnReady = function() {
