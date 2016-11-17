0105b0cd016bc58f6d330a01c647769f1f6ad5e2
Dave Stanton <davestanton.us@gmail.com>
2016-11-17 18:22:04 -0500

    Chapter 3 - Controlling navigation part 1.

--- public/app.js
+++ public/app.js
@@ -17,6 +17,10 @@ learnjs.problems = [
 
 // Views
 
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
@@ -31,7 +35,9 @@ learnjs.problemView = function(data) {
 
   function checkAnswerClick() {
     if (checkAnswer()) {
-      learnjs.flashElement(resultFlash, 'Correct!');
+      var correctFlash = learnjs.template('correct-flash');
+      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+      learnjs.flashElement(resultFlash, correctFlash);
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
